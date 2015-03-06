182643da9b8c3cd0a67bcb291e8124bb6457c0fd
timwoj <timwoj@ieee.org>
2015-03-06 14:00:25 -0700

    Fix some versioning issues with older packages

--- apt_packages.sh
+++ apt_packages.sh
@@ -3,16 +3,19 @@
 apt-get update
 apt-get install -y rubygems python-twisted git libxml2-dev libxslt-dev
 
-# mark the ruby package as hold, so that apt won't update it.  if it updates, it gets pushed to
-# ruby 1.9, which we don't want.
+# mark the ruby package as hold, so that apt won't update it.  if it updates,
+# it gets pushed to ruby 1.9, which we don't want.
 apt-mark hold ruby
 
-# this extra stuff here allows apt-get to upgrade all of the packages without needing user input.
-# grub always wants to pop up a menu and cause problems.
+# this extra stuff here allows apt-get to upgrade all of the packages without
+# needing user input.  grub always wants to pop up a menu and cause problems.
 export DEBIAN_FRONTEND=noninteractive
 #apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
-# Insatll bundler, gem, and a specific version of rails that's known to work.
+# Install bundler, rake, and specific versions of some of the gems that are
+# either known to work with ShC or work-around specific problems in the
+# vagrant provisioning.
 gem install bundler
 gem install rake
+gem install i18n --version 0.6.11
 gem install rails --version 3.2.19
